4744559b46df8d91e51067a501773a00acbcc567
Andrey Prygunkov <hugbug@users.noreply.github.com>
2008-01-30 17:06:20 +0000

    removed <-testing> from version-string (prepare for release 0.3.1)

--- win32.h
+++ win32.h
@@ -52,7 +52,7 @@
 /* Define to 1 if variadic macros are supported */
 #define HAVE_VARIADIC_MACROS
 
-#define VERSION "0.3.1-testing"
+#define VERSION "0.3.1"
 
 /* Suppress warnings */
 #define _CRT_SECURE_NO_DEPRECATE
