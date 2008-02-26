c16544d8a17172456372e0009125a25a0888a658
hugbug <hugbug@b1b436ca-2b18-4f33-ab5d-510789358d54>
2008-02-26 17:21:01 +0000

    changed version string to <0.4.0-testing>

--- trunk/win32.h
+++ trunk/win32.h
@@ -49,7 +49,7 @@
 /* Define to 1 if variadic macros are supported */
 #define HAVE_VARIADIC_MACROS
 
-#define VERSION "0.3.2-testing"
+#define VERSION "0.4.0-testing"
 
 /* Suppress warnings */
 #define _CRT_SECURE_NO_DEPRECATE
