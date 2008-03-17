173622fea1ee6811c83a50b2b870338bab12c04f
Andrey Prygunkov <hugbug@users.noreply.github.com>
2008-03-17 16:44:14 +0000

    implemented the output-redirection for post-process-scripts on windows

--- nzbget.h
+++ nzbget.h
@@ -34,15 +34,16 @@
 #ifndef strdup
 #define strdup _strdup
 #endif
+#define fdopen _fdopen
 #define ctime_r(timep, buf, bufsize) ctime_s(buf, bufsize, timep)
 #define int32_t __int32
 #define mkdir(dir, flags) _mkdir(dir)
 #define strcasecmp(a, b) _stricmp(a, b)
 #define strncasecmp(a, b, c) _strnicmp(a, b, c)
 
-#pragma warning(disable:4800)
-#pragma warning(disable:4267)
-#pragma warning(disable:4244)
+#pragma warning(disable:4800) // 'type' : forcing value to bool 'true' or 'false' (performance warning)
+#pragma warning(disable:4267) // 'var' : conversion from 'size_t' to 'type', possible loss of data
+#pragma warning(disable:4244) // 'argument' : conversion from 'type1' to 'type2', possible loss of data
 
 #define	__S_ISTYPE(mode, mask)	(((mode) & _S_IFMT) == (mask))
 #define	S_ISDIR(mode)	 __S_ISTYPE((mode), _S_IFDIR)
