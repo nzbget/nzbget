512e9789d9a30ce42dafd5516272decec60a288c
Andrey Prygunkov <hugbug@users.noreply.github.com>
2008-07-21 20:49:07 +0000

    fixed: seg.fault by queueing the files from the top-level incoming directory (bug introduced in r191)

--- PrePostProcessor.cpp
+++ PrePostProcessor.cpp
@@ -146,7 +146,7 @@ void PrePostProcessor::Run()
 			iNZBDirInterval >= g_pOptions->GetNzbDirInterval() * 1000)
 		{
 			// check nzbdir every g_pOptions->GetNzbDirInterval() seconds
-			CheckIncomingNZBs(g_pOptions->GetNzbDir(), NULL);
+			CheckIncomingNZBs(g_pOptions->GetNzbDir(), "");
 			iNZBDirInterval = 0;
 		}
 		iNZBDirInterval += 200;
@@ -283,7 +283,7 @@ void PrePostProcessor::CheckIncomingNZBs(const char* szDirectory, const char* sz
 			int len = strlen(filename);
 
 			// check subfolders of first level
-			if (!szCategory && (buffer.st_mode & S_IFDIR) != 0 && strcmp(filename, ".") && strcmp(filename, ".."))
+			if (strlen(szCategory) > 0 && (buffer.st_mode & S_IFDIR) != 0 && strcmp(filename, ".") && strcmp(filename, ".."))
 			{
 				fullfilename[strlen(fullfilename) + 1] = '\0';
 				fullfilename[strlen(fullfilename)] = PATH_SEPARATOR;
