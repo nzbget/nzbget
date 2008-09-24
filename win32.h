b338a4bba38c71de98ae3d67d6511ee995bebfbf
Andrey Prygunkov <hugbug@users.noreply.github.com>
2008-09-24 16:45:24 +0000

    updated version string

--- win32.h
+++ win32.h
@@ -54,7 +54,7 @@
 /* Define to 1 if variadic macros are supported */
 #define HAVE_VARIADIC_MACROS
 
-#define VERSION "0.5.1"
+#define VERSION "0.6.0-testing"
 
 /* Suppress warnings */
 #define _CRT_SECURE_NO_DEPRECATE
