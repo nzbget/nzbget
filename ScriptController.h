a3a56b2ee9a0228e454252f29fa9d1af6e962069
Andrey Prygunkov <hugbug@users.noreply.github.com>
2009-04-19 17:26:41 +0000

    fixed compilation warning on certain versions of gcc

--- ScriptController.h
+++ ScriptController.h
@@ -1,7 +1,7 @@
 /*
  *  This file is part of nzbget
  *
- *  Copyright (C) 2007-2008 Andrei Prygounkov <hugbug@users.sourceforge.net>
+ *  Copyright (C) 2007-2009 Andrei Prygounkov <hugbug@users.sourceforge.net>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -77,6 +77,7 @@ protected:
 
 public:
 						ScriptController();
+	virtual				~ScriptController() {};
 	int					Execute();
 	void				Terminate();
 
