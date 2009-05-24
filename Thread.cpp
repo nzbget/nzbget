1866295d5d4ae7a9a0cee8c8bb4f2d7aa97ca735
Andrey Prygunkov <hugbug@users.noreply.github.com>
2009-05-24 17:03:34 +0000

    refactoring: removed references to OS-specific includes and types from <Thread.h> in order to isolate other units from these specifics

--- Thread.cpp
+++ Thread.cpp
@@ -1,8 +1,8 @@
 /*
- *  This file if part of nzbget
+ *  This file is part of nzbget
  *
- *  Copyright (C) 2004  Sven Henkel <sidddy@users.sourceforge.net>
- *  Copyright (C) 2007  Andrei Prygounkov <hugbug@users.sourceforge.net>
+ *  Copyright (C) 2004 Sven Henkel <sidddy@users.sourceforge.net>
+ *  Copyright (C) 2007-2009 Andrei Prygounkov <hugbug@users.sourceforge.net>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -40,13 +40,15 @@
 #include <process.h>
 #else
 #include <fcntl.h>
+#include <pthread.h>
+#include <semaphore.h>
 #endif
 
 #include "Log.h"
 #include "Thread.h"
 
 int Thread::m_iThreadCount = 1; // take the main program thread into account
-Mutex Thread::m_mutexThread;
+Mutex* Thread::m_pMutexThread;
 
 #ifndef WIN32
 #ifndef HAVE_UNNAMED_SEMAPHORES	
@@ -58,44 +60,47 @@ Mutex Semaphore::m_mutexID;
 Mutex::Mutex()
 {
 #ifdef WIN32
-	InitializeCriticalSection(&m_mutexObj);
+	m_pMutexObj = (CRITICAL_SECTION*)malloc(sizeof(CRITICAL_SECTION));
+	InitializeCriticalSection((CRITICAL_SECTION*)m_pMutexObj);
 #else
-	pthread_mutex_init(&m_mutexObj, NULL);
+	m_pMutexObj = (pthread_mutex_t*)malloc(sizeof(pthread_mutex_t));
+	pthread_mutex_init((pthread_mutex_t*)m_pMutexObj, NULL);
 #endif
 }
 
 Mutex::~ Mutex()
 {
 #ifdef WIN32
-	DeleteCriticalSection(&m_mutexObj);
+	DeleteCriticalSection((CRITICAL_SECTION*)m_pMutexObj);
 #else
-	pthread_mutex_destroy(&m_mutexObj);
+	pthread_mutex_destroy((pthread_mutex_t*)m_pMutexObj);
 #endif
+	free(m_pMutexObj);
 }
 
 void Mutex::Lock()
 {
 #ifdef WIN32
-	EnterCriticalSection(&m_mutexObj);
+	EnterCriticalSection((CRITICAL_SECTION*)m_pMutexObj);
 #ifdef DEBUG
 	// CriticalSections on Windows can be locked many times from the same thread,
 	// but we do not want this and must treat such situations as errors and detect them.
-	if (m_mutexObj.RecursionCount > 1)
+	if (((CRITICAL_SECTION*)m_pMutexObj)->RecursionCount > 1)
 	{
 		error("Internal program error: inconsistent thread-lock detected");
 	}
 #endif
 #else
-	pthread_mutex_lock(&m_mutexObj);
+	pthread_mutex_lock((pthread_mutex_t*)m_pMutexObj);
 #endif
 }
 
 void Mutex::Unlock()
 {
 #ifdef WIN32
-	LeaveCriticalSection(&m_mutexObj);
+	LeaveCriticalSection((CRITICAL_SECTION*)m_pMutexObj);
 #else
-	pthread_mutex_unlock(&m_mutexObj);
+	pthread_mutex_unlock((pthread_mutex_t*)m_pMutexObj);
 #endif
 }
 
@@ -103,7 +108,7 @@ void Mutex::Unlock()
 Semaphore::Semaphore()
 {
 #ifdef WIN32
-	m_semObj = CreateSemaphore(NULL, 0, 1, NULL);
+	m_pSemObj = CreateSemaphore(NULL, 0, 1, NULL);
 #else
 	CreateSemObj(0);
 #endif
@@ -112,7 +117,7 @@ Semaphore::Semaphore()
 Semaphore::Semaphore(int iValue)
 {
 #ifdef WIN32
-	m_semObj = CreateSemaphore(NULL, iValue, iValue, NULL);
+	m_pSemObj = CreateSemaphore(NULL, iValue, iValue, NULL);
 #else
 	CreateSemObj(iValue);
 #endif
@@ -122,8 +127,8 @@ Semaphore::Semaphore(int iValue)
 void Semaphore::CreateSemObj(int iValue)
 {
 #ifdef HAVE_UNNAMED_SEMAPHORES
-	m_semObj = (sem_t*)malloc(sizeof(sem_t));
-	int iRet = sem_init(m_semObj, 0, iValue);
+	m_pSemObj = (sem_t*)malloc(sizeof(sem_t));
+	int iRet = sem_init((sem_t*)m_pSemObj, 0, iValue);
 	if (iRet == -1)
 	{
 		error("sem_init failed: error %i", errno);
@@ -134,8 +139,8 @@ void Semaphore::CreateSemObj(int iValue)
 	m_iID++;
 	m_mutexID.Unlock();
 	snprintf(m_szName, sizeof(m_szName), "/NZBGET-%i", iID);
-	m_semObj = sem_open(m_szName, O_CREAT, S_IRWXU, iValue);
-	if (m_semObj == SEM_FAILED)
+	m_pSemObj = sem_open(m_szName, O_CREAT, S_IRWXU, iValue);
+	if (m_pSemObj == SEM_FAILED)
 	{
 		error("sem_open failed: error %i", errno);
 	}
@@ -146,13 +151,13 @@ void Semaphore::CreateSemObj(int iValue)
 Semaphore::~ Semaphore()
 {
 #ifdef WIN32
-	CloseHandle(m_semObj);
+	CloseHandle((HANDLE)m_pSemObj);
 #else
 #ifdef HAVE_UNNAMED_SEMAPHORES	
-	sem_destroy(m_semObj);
-	free(m_semObj);
+	sem_destroy((sem_t*)m_pSemObj);
+	free(m_pSemObj);
 #else
-	sem_close(m_semObj);
+	sem_close((sem_t*)m_pSemObj);
 	sem_unlink(m_szName);
 #endif
 #endif
@@ -161,36 +166,55 @@ Semaphore::~ Semaphore()
 void Semaphore::Post()
 {
 #ifdef WIN32
-	ReleaseSemaphore(m_semObj, 1, NULL);
+	ReleaseSemaphore((HANDLE)m_pSemObj, 1, NULL);
 #else
-	sem_post(m_semObj);
+	sem_post((sem_t*)m_pSemObj);
 #endif
 }
 
 bool Semaphore::Wait()
 {
 #ifdef WIN32
-	return WaitForSingleObject(m_semObj, INFINITE) == WAIT_OBJECT_0;
+	return WaitForSingleObject((HANDLE)m_pSemObj, INFINITE) == WAIT_OBJECT_0;
 #else
-	return sem_wait(m_semObj) == 0;
+	return sem_wait((sem_t*)m_pSemObj) == 0;
 #endif
 }
 
 bool Semaphore::TryWait()
 {
 #ifdef WIN32
-	return WaitForSingleObject(m_semObj, 0) == WAIT_OBJECT_0;
+	return WaitForSingleObject((HANDLE)m_pSemObj, 0) == WAIT_OBJECT_0;
 #else
-	return sem_trywait(m_semObj) == 0;
+	return sem_trywait((sem_t*)m_pSemObj) == 0;
 #endif
 }
 
+
+void Thread::Init()
+{
+	debug("Initializing global thread data");
+
+	m_pMutexThread = new Mutex();
+}
+
+void Thread::Final()
+{
+	debug("Finalizing global thread data");
+
+	delete m_pMutexThread;
+}
 
 Thread::Thread()
 {
 	debug("Creating Thread");
 
-	m_Thread = 0;
+#ifdef WIN32
+	m_pThreadObj = NULL;
+#else
+	m_pThreadObj = (pthread_t*)malloc(sizeof(pthread_t));
+	*((pthread_t*)m_pThreadObj) = 0;
+#endif
 	m_bRunning = false;
 	m_bStopped = false;
 	m_bAutoDestroy = false;
@@ -199,6 +223,9 @@ Thread::Thread()
 Thread::~Thread()
 {
 	debug("Destroying Thread");
+#ifndef WIN32
+	free(m_pThreadObj);
+#endif
 }
 
 void Thread::Start()
@@ -207,30 +234,29 @@ void Thread::Start()
 
 	m_bRunning = true;
 	
-	// NOTE: we must garantee, that in a time we setting m_bRunning
+	// NOTE: we must guarantee, that in a time we set m_bRunning
 	// to value returned from pthread_create, the thread-object still exists.
 	// This is not obviously!
 	// pthread_create could wait long enough before returning result
-	// back to allow the started thread to complete it job
-	// and terminate.
-	// We lock mutex m_mutexThread on calling pthread_create; the started thread
+	// back to allow the started thread to complete its job and terminate.
+	// We lock mutex m_pMutexThread on calling pthread_create; the started thread
 	// then also try to lock the mutex (see thread_handler) and therefore
 	// must wait until we unlock it
-	m_mutexThread.Lock();
+	m_pMutexThread->Lock();
 
 #ifdef WIN32
-	m_Thread = (HANDLE)_beginthread(Thread::thread_handler, 0, (void *)this);
-	m_bRunning = m_Thread != NULL;
+	m_pThreadObj = (HANDLE)_beginthread(Thread::thread_handler, 0, (void *)this);
+	m_bRunning = m_pThreadObj != NULL;
 #else
 	pthread_attr_t m_Attr;
 	pthread_attr_init(&m_Attr);
 	pthread_attr_setdetachstate(&m_Attr, PTHREAD_CREATE_DETACHED);
 	pthread_attr_setinheritsched(&m_Attr , PTHREAD_INHERIT_SCHED);
-	m_bRunning = !pthread_create(&m_Thread, &m_Attr, Thread::thread_handler, (void *) this);
+	m_bRunning = !pthread_create((pthread_t*)m_pThreadObj, &m_Attr, Thread::thread_handler, (void *) this);
 	pthread_attr_destroy(&m_Attr);
 #endif
 
-	m_mutexThread.Unlock();
+	m_pMutexThread->Unlock();
 }
 
 void Thread::Stop()
@@ -244,19 +270,19 @@ bool Thread::Kill()
 {
 	debug("Killing Thread");
 
-	m_mutexThread.Lock();
+	m_pMutexThread->Lock();
 
 #ifdef WIN32
-	bool terminated = TerminateThread(m_Thread, 0) != 0;
+	bool terminated = TerminateThread((HANDLE)m_pThreadObj, 0) != 0;
 #else
-	bool terminated = pthread_cancel(m_Thread) == 0;
+	bool terminated = pthread_cancel(*(pthread_t*)m_pThreadObj) == 0;
 #endif
 
 	if (terminated)
 	{
 		m_iThreadCount--;
 	}
-	m_mutexThread.Unlock();
+	m_pMutexThread->Unlock();
 	return terminated;
 }
 
@@ -266,9 +292,9 @@ void __cdecl Thread::thread_handler(void* pObject)
 void* Thread::thread_handler(void* pObject)
 #endif
 {
-	m_mutexThread.Lock();
+	m_pMutexThread->Lock();
 	m_iThreadCount++;
-	m_mutexThread.Unlock();
+	m_pMutexThread->Unlock();
 
 	debug("Entering Thread-func");
 
@@ -286,9 +312,9 @@ void* Thread::thread_handler(void* pObject)
 		delete pThread;
 	}
 
-	m_mutexThread.Lock();
+	m_pMutexThread->Lock();
 	m_iThreadCount--;
-	m_mutexThread.Unlock();
+	m_pMutexThread->Unlock();
 
 #ifndef WIN32
 	return NULL;
@@ -297,8 +323,8 @@ void* Thread::thread_handler(void* pObject)
 
 int Thread::GetThreadCount()
 {
-	m_mutexThread.Lock();
+	m_pMutexThread->Lock();
 	int iThreadCount = m_iThreadCount;
-	m_mutexThread.Unlock();
+	m_pMutexThread->Unlock();
 	return iThreadCount;
 }
