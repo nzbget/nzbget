7ce8c0b9665dd983d784475d8cb1d0ba83ba6f6b
Andrey Prygunkov <hugbug@users.noreply.github.com>
2012-04-29 16:15:25 +0000

    updated version string, ChangeLog and README (preparing to release 0.8.0)

--- win32.h
+++ win32.h
@@ -66,7 +66,7 @@
 /* Define to 1 if spinlocks are supported */
 #define HAVE_SPINLOCK
 
-#define VERSION "0.8.0-testing"
+#define VERSION "0.8.0"
 
 /* Suppress warnings */
 #define _CRT_SECURE_NO_DEPRECATE
