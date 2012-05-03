87793b3dc31308f51cfd265cbc1bcafa720af21a
Andrey Prygunkov <hugbug@users.noreply.github.com>
2012-05-03 10:23:00 +0000

    updated version string to 0.9.0-testing

--- win32.h
+++ win32.h
@@ -66,7 +66,7 @@
 /* Define to 1 if spinlocks are supported */
 #define HAVE_SPINLOCK
 
-#define VERSION "0.8.0"
+#define VERSION "0.9.0-testing"
 
 /* Suppress warnings */
 #define _CRT_SECURE_NO_DEPRECATE
