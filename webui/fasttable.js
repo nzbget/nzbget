f604460d562056e8cb0020df9bde51034f13f56a
Andrey Prygunkov <hugbug@users.noreply.github.com>
2012-08-06 20:32:48 +0000

    set svn keywords

--- webui/fasttable.js
+++ webui/fasttable.js
@@ -17,8 +17,8 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * $Revision: 1 $
- * $Date: 2012-05-01 00:00:00 +0200 (Di, 01 Mai 2012) $
+ * $Revision$
+ * $Date$
  *
  */
 
