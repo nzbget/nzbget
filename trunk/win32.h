5831dc5b0a5a11dc9413a11c001eda20ca7a6dd1
hugbug <hugbug@b1b436ca-2b18-4f33-ab5d-510789358d54>
2013-04-06 12:58:50 +0000

    updated version string to 11.0-testing

--- trunk/win32.h
+++ trunk/win32.h
@@ -74,7 +74,7 @@
 /* Define to 1 if spinlocks are supported */
 #define HAVE_SPINLOCK
 
-#define VERSION "10.1"
+#define VERSION "11.0-testing"
 
 /* Suppress warnings */
 #define _CRT_SECURE_NO_DEPRECATE
