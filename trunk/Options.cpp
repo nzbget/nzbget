152551fdc9e6f292be429b4b6ef19f14c05aaf5c
hugbug <hugbug@b1b436ca-2b18-4f33-ab5d-510789358d54>
2013-07-15 22:27:14 +0000

    made alias-matching case-insensitive

--- trunk/Options.cpp
+++ trunk/Options.cpp
@@ -383,7 +383,18 @@ Options::Category* Options::Categories::FindCategory(const char* szName, bool bS
 			for (NameList::iterator it2 = pCategory->GetAliases()->begin(); it2 != pCategory->GetAliases()->end(); it2++)
 			{
 				const char* szAlias = *it2;
-				if (Util::MatchMask(szName, szAlias))
+
+				char szLowerAlias[1024];
+				strncpy(szLowerAlias, szAlias, sizeof(szLowerAlias));
+				szLowerAlias[1024-1] = '\0';
+				for (char* p = szLowerAlias; *p; p++) *p = tolower(*p); // convert string to lowercase
+
+				char szLowerName[1024];
+				strncpy(szLowerName, szName, sizeof(szLowerName));
+				szLowerName[1024-1] = '\0';
+				for (char* p = szLowerName; *p; p++) *p = tolower(*p); // convert string to lowercase
+				
+				if (Util::MatchMask(szLowerName, szLowerAlias))
 				{
 					return pCategory;
 				}
