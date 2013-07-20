e5b9a8f828d1ce6c5eea8238c159cbb4127e718d
hugbug <hugbug@b1b436ca-2b18-4f33-ab5d-510789358d54>
2013-07-20 14:05:21 +0000

    fixed compilation error on Linux (bug introduced in r743)

--- trunk/NNTPConnection.cpp
+++ trunk/NNTPConnection.cpp
@@ -2,7 +2,7 @@
  *  This file is part of nzbget
  *
  *  Copyright (C) 2004 Sven Henkel <sidddy@users.sourceforge.net>
- *  Copyright (C) 2007-2008 Andrey Prygunkov <hugbug@users.sourceforge.net>
+ *  Copyright (C) 2007-2013 Andrey Prygunkov <hugbug@users.sourceforge.net>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -35,6 +35,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <ctype.h>
 
 #include "nzbget.h"
 #include "Log.h"
