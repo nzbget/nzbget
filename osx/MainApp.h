be37a75928889842eaed98ae63718bf0215c4153
Andrey Prygunkov <hugbug@users.noreply.github.com>
2013-08-31 21:14:39 +0000

    set svn keywords

--- osx/MainApp.h
+++ osx/MainApp.h
@@ -17,8 +17,8 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * $Revision: 0 $
- * $Date: 2013-08-19 12:00:00 +0200 (Sa, 19 Aug 2013) $
+ * $Revision$
+ * $Date$
  *
  */
 
