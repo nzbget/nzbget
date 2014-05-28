c8328bb6c7c61ec0759870e70d3999d763b6df3d
hugbug <hugbug@b1b436ca-2b18-4f33-ab5d-510789358d54>
2014-05-28 22:19:39 +0000

    post-processing scripts now have two new parameters: env. var "NZBPP_STATUS" indicates the status of download including the total status (SUCCESS, FAILURE, etc.) and the detail field (for example in case of failures: PAR, UNPACK, etc.); env. var "NZBPP_TOTALSTATUS" is equal to the total status of parameter "NZBPP_STATUS" and is provided for convenience (to avoid parsing of "NZBPP_STATUS"); the new parameters provide a simple way for pp-scripts to determine download status without a guess work needed in previous versions; parameters "NZBPP_PARSTATUS" and "NZBPP_UNPACKSTATUS" are now considered deprecated (still passed for compatibility); updated script "EMail.py" to use new parameters "NZBPP_TOTALSTATUS" and "NZBPP_STATUS" instead of "NZBPP_PARSTATUS" and "NZBPP_UNPACKSTATUS"

--- trunk/scripts/EMail.py
+++ trunk/scripts/EMail.py
@@ -2,7 +2,7 @@
 #
 # E-Mail post-processing script for NZBGet
 #
-# Copyright (C) 2013 Andrey Prygunkov <hugbug@users.sourceforge.net>
+# Copyright (C) 2013-2014 Andrey Prygunkov <hugbug@users.sourceforge.net>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -107,41 +107,15 @@ for	optname in required_options:
 		print('[ERROR] Option %s is missing in configuration file. Please check script settings' % optname[6:])
 		sys.exit(POSTPROCESS_ERROR)
 		
-# Check par and unpack status for errors.
-success=False
-if os.environ['NZBPP_PARSTATUS'] == '1' or os.environ['NZBPP_UNPACKSTATUS'] == '1':
-	subject = 'Failure for "%s"' % (os.environ['NZBPP_NZBNAME'])
-	text = 'Download of "%s" has failed.' % (os.environ['NZBPP_NZBNAME'])
-elif os.environ['NZBPP_UNPACKSTATUS'] in ('3', '4'):
-	subject = 'Unpack failure for "%s"' % (os.environ['NZBPP_NZBNAME'])
-	text = 'Download of "%s" has failed.' % (os.environ['NZBPP_NZBNAME'])
-elif os.environ['NZBPP_PARSTATUS'] == '4':
-	subject = 'Damaged for "%s"' % (os.environ['NZBPP_NZBNAME'])
-	text = 'Download of "%s" requires par-repair.' % (os.environ['NZBPP_NZBNAME'])
-else:
+success = os.environ['NZBPP_TOTALSTATUS'] == 'SUCCESS'
+if success:
 	subject = 'Success for "%s"' % (os.environ['NZBPP_NZBNAME'])
 	text = 'Download of "%s" has successfully completed.' % (os.environ['NZBPP_NZBNAME'])
-	success=True
+else:
+	subject = 'Failure for "%s"' % (os.environ['NZBPP_NZBNAME'])
+	text = 'Download of "%s" has failed.' % (os.environ['NZBPP_NZBNAME'])
 
-#  NZBPP_PARSTATUS    - result of par-check:
-#                       0 = not checked: par-check is disabled or nzb-file does
-#                           not contain any par-files;
-#                       1 = checked and failed to repair;
-#                       2 = checked and successfully repaired;
-#                       3 = checked and can be repaired but repair is disabled;
-#                       4 = par-check needed but skipped (option ParCheck=manual);
-parStatus = { '0': 'skipped', '1': 'failed', '2': 'repaired', '3': 'repairable', '4': 'manual' }
-text += '\nPar-Status: %s' % parStatus[os.environ['NZBPP_PARSTATUS']]
-
-#  NZBPP_UNPACKSTATUS - result of unpack:
-#                       0 = unpack is disabled or was skipped due to nzb-file
-#                           properties or due to errors during par-check;
-#                       1 = unpack failed;
-#                       2 = unpack successful;
-#                       3 = write error (usually not enough disk space);
-#                       4 = wrong password (only for rar5 archives);
-unpackStatus = { '0': 'skipped', '1': 'failed', '2': 'success', '3': 'write error (usually not enough disk space)', '4': 'wrong password' }
-text += '\nUnpack-Status: %s' % unpackStatus[os.environ['NZBPP_UNPACKSTATUS']]
+text += '\nStatus: %s' % os.environ['NZBPP_STATUS']
 
 # add list of downloaded files
 if os.environ['NZBPO_FILELIST'] == 'yes':
