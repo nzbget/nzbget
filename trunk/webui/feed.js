91ca42a01ad2ffb82a6cd839296fbed02787fe59
hugbug <hugbug@b1b436ca-2b18-4f33-ab5d-510789358d54>
2015-02-22 22:05:59 +0000

    added a small button near feed name in the feed menu on downloads-page; a click on the button fetches the feed, whereas a click on the feed title shows feed's content (as before)

--- trunk/webui/feed.js
+++ trunk/webui/feed.js
@@ -1,7 +1,7 @@
 /*
  *  This file is part of nzbget
  *
- *  Copyright (C) 2013 Andrey Prygunkov <hugbug@users.sourceforge.net>
+ *  Copyright (C) 2013-2015 Andrey Prygunkov <hugbug@users.sourceforge.net>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -60,10 +60,13 @@ var Feeds = (new function($)
 			{
 				var item = menuItemTemplate.clone();
 				var name = Options.option('Feed' + i + '.Name');
-				var a = $('a', item);
+				var a = $('span', item);
 				a.text(name !== '' ? name : 'Feed' + i);
 				a.attr('data-id', i);
 				a.click(viewFeed);
+				var im = $('button', item);
+				im.click(fetchFeed);
+				im.attr('data-id', i);
 				insertPos.before(item);
 			}
 		}
@@ -77,11 +80,20 @@ var Feeds = (new function($)
 		FeedDialog.showModal(id);
 	}
 
+	function fetchFeed()
+	{
+		var id = parseInt($(this).attr('data-id'));
+		RPC.call('fetchfeed', [id], function()
+		{
+			Notification.show('#Notif_Feeds_Fetch');
+		});
+	}
+
 	this.fetchAll = function()
 	{
 		RPC.call('fetchfeed', [0], function()
 		{
-			Notification.show('#Notif_Feeds_FetchAll');
+			Notification.show('#Notif_Feeds_Fetch');
 		});
 	}
 }(jQuery));
