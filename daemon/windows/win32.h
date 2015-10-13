5356f5aafcf3278a52663492559d8679883f177d
Andrey Prygunkov <hugbug@users.sourceforge.net>
2015-10-13 23:22:19 +0200

    updated version string to 17.0-testing

--- daemon/windows/win32.h
+++ daemon/windows/win32.h
@@ -68,7 +68,7 @@
 /* Define to 1 if you have the <regex.h> header file. */
 #define HAVE_REGEX_H 1
 
-#define VERSION "16.0"
+#define VERSION "17.0-testing"
 
 /* Suppress warnings */
 #define _CRT_SECURE_NO_DEPRECATE
