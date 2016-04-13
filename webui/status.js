a4a3d1bc7a987897262166f129a1ec9e22b6ca60
Andrey Prygunkov <hugbug@users.sourceforge.net>
2016-04-13 22:22:01 +0200

    #99: indication of reached quota in web-interface
     - remaining time is shown in orange when the quota is reached;
     - dialog “statistics and status” may show extra row “Download quota:
    reached”.

--- webui/status.js
+++ webui/status.js
@@ -178,9 +178,13 @@ var Status = (new function($)
 
 		$StatusSpeedIcon.toggleClass('icon-plane', !limit);
 		$StatusSpeedIcon.toggleClass('icon-truck', limit);
-		$StatusTime.toggleClass('scheduled-resume', status.ServerStandBy && status.ResumeTime > 0);
-		$StatusTimeIcon.toggleClass('icon-time', !(status.ServerStandBy && status.ResumeTime > 0));
-		$StatusTimeIcon.toggleClass('icon-time-orange', status.ServerStandBy && status.ResumeTime > 0);
+
+		var statWarning = (status.ServerStandBy && status.ResumeTime > 0) ||
+			(parseFloat(Options.option('MonthlyQuota')) > 0 && Status.status.MonthSizeMB >= parseFloat(Options.option('MonthlyQuota'))) ||
+			(parseFloat(Options.option('DayQuota')) > 0 && Status.status.DaySizeMB >= parseFloat(Options.option('DayQuota')));
+		$StatusTime.toggleClass('orange', statWarning);
+		$StatusTimeIcon.toggleClass('icon-time', !statWarning);
+		$StatusTimeIcon.toggleClass('icon-time-orange', statWarning);
 	}
 
 	function updatePlayButton()
@@ -574,6 +578,12 @@ var StatDialog = (new function($)
 			'<span class="label label-status label-warning">paused</span>' :
 			'<span class="label label-status label-success">active</span>')) +
 			'</td></tr>';
+
+		if ((parseFloat(Options.option('MonthlyQuota')) > 0 && Status.status.MonthSizeMB >= parseFloat(Options.option('MonthlyQuota'))) ||
+			(parseFloat(Options.option('DayQuota')) > 0 && Status.status.DaySizeMB >= parseFloat(Options.option('DayQuota'))))
+		{
+			content += '<tr><td>Download quota</td><td class="text-right"><span class="label label-status label-warning">reached</span></td></tr>';
+		}
 
 		if (status.ResumeTime > 0)
 		{
