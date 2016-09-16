f8920b4b9e81d3a682b6a94da0fbd1f15e3e3936
Lawrence Matacena <lawrence.matacena@gmail.com>
2016-09-16 15:32:15 -0400

    added title attribute to make note of modifier

--- webui/index.js
+++ webui/index.js
@@ -240,6 +240,7 @@ var Frontend = (new function($)
 		setupSearch();
 		
 		$('li > a:has(table)').addClass('has-table');
+		$('table.table-cancheck th .check').attr({ title: 'Hold Ctrl or Windows/Command key to apply action to ALL items' });
 
 		$(document).on("keypress", "form", function(event)
 		{
