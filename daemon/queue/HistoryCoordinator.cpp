4ca95b2989022f7d46fec0c1c1141de76056c310
Andrey Prygunkov <hugbug@users.sourceforge.net>
2017-05-22 22:05:52 +0200

    #371: reset direct unpack status on post-process again

--- daemon/queue/HistoryCoordinator.cpp
+++ daemon/queue/HistoryCoordinator.cpp
@@ -1,7 +1,7 @@
 /*
  *  This file is part of nzbget. See <http://nzbget.net>.
  *
- *  Copyright (C) 2007-2016 Andrey Prygunkov <hugbug@users.sourceforge.net>
+ *  Copyright (C) 2007-2017 Andrey Prygunkov <hugbug@users.sourceforge.net>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -384,6 +384,7 @@ void HistoryCoordinator::MoveToQueue(DownloadQueue* downloadQueue, HistoryList::
 	if (!nzbInfo->GetUnpackCleanedUpDisk())
 	{
 		nzbInfo->SetUnpackStatus(NzbInfo::usNone);
+		nzbInfo->SetDirectUnpackStatus(NzbInfo::nsNone);
 		nzbInfo->SetCleanupStatus(NzbInfo::csNone);
 		nzbInfo->SetParRenameStatus(NzbInfo::rsNone);
 		nzbInfo->SetRarRenameStatus(NzbInfo::rsNone);
