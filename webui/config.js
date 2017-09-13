8545cb35818f0877841a43019d6ba6931c21db53
Andrey Prygunkov <hugbug@users.sourceforge.net>
2017-09-13 18:39:40 +0200

    #426: show unpack password as plain field

--- webui/config.js
+++ webui/config.js
@@ -826,8 +826,9 @@ var Config = (new function($)
 				'</div>';
 		}
 		else if (option.caption.toLowerCase().indexOf('password') > -1 &&
-			option.caption.toLowerCase() !== '*unpack:password')
+			option.name.toLowerCase() !== '*unpack:password')
 		{
+			console.log(option.caption);
 			option.type = 'password';
 			html += '<div class="password-field input-append">' +
 				'<input type="password" id="' + option.formId + '" value="' + Util.textToAttr(value) + '" class="editsmall">'+
@@ -839,7 +840,7 @@ var Config = (new function($)
 				'</div>';
 		}
 		else if (option.caption.toLowerCase().indexOf('username') > -1 ||
-			(option.caption.indexOf('IP') > -1 && option.caption.toLowerCase() !== 'authorizedip'))
+			(option.caption.indexOf('IP') > -1 && option.name.toLowerCase() !== 'authorizedip'))
 		{
 			option.type = 'text';
 			html += '<input type="text" id="' + option.formId + '" value="' + Util.textToAttr(value) + '" class="editsmall">';
