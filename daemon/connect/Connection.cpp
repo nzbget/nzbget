2bdc87c1986c9542633445c043b3b7484f8c8674
Andrey Prygunkov <hugbug@users.sourceforge.net>
2017-09-18 17:49:12 +0200

    fixed #445: program hangs during update on Linux

--- daemon/connect/Connection.cpp
+++ daemon/connect/Connection.cpp
@@ -883,21 +883,19 @@ void Connection::Cancel()
 	debug("Cancelling connection");
 	if (m_socket != INVALID_SOCKET)
 	{
-		if (m_status == csListening)
+		EStatus status = m_status;
+		m_status = csCancelled;
+
+		if (status == csListening)
 		{
-			m_status = csCancelled;
 			SOCKET socket = m_socket;
 			m_socket = INVALID_SOCKET;
+			shutdown(socket, SHUT_RDWR);
 			closesocket(socket);
 		}
 		else
 		{
-			m_status = csCancelled;
-			int r = shutdown(m_socket, SHUT_RDWR);
-			if (r == -1)
-			{
-				ReportError("Could not shutdown connection for %s", m_host, true);
-			}
+			shutdown(m_socket, SHUT_RDWR);
 		}
 	}
 }
